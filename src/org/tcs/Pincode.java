9de24bef00e2d5ad67921bd4581b9a55ed2f7dde
Vengadesh97 <vengadeshw45@gmail.com>
2021-10-04 15:07:30 +0530

    Function

--- src/org/tcs/Pincode.java
+++ src/org/tcs/Pincode.java
@@ -2,8 +2,13 @@ package org.tcs;
 
 public class Pincode {
 	
+	public  void number() {
+     System.out.println("number");
+	}
+	
 	public static void main(String[] args) {
-		
+		Pincode p = new Pincode();
+		p.number();
 	}
 
 }
